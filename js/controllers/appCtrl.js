e5eae9976dc5021a3a1e0d90697f04fae8fa574b
BeaMorim <biadmorim@gmail.com>
2017-12-16 13:03:40 -0200

    Commit

--- js/controllers/appCtrl.js
+++ js/controllers/appCtrl.js
@@ -1,9 +1,11 @@
 app.controller("appCtrl", function ($scope, chatbotService) {
-    $scope.createChat = function() {
-
+    $scope.createChat = function () {
+        document.getElementById("chatbot").innerHTML += `<div class='question'>${$scope.question}</div>`
         chatbotService.chat($scope.question)
-            .then(function(promisse) {
+            .then(function (promisse) {
                 $scope.answer = promisse.data.answers[0].answer;
+                document.getElementById("chatbot").innerHTML += `<div id="last-one" class='answer'>${$scope.answer}</div>`
             })
+        $scope.question = "";
     }
 })
